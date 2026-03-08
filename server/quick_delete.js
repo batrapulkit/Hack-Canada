2cbde3196d54ddb1fd1f54eaaefeac4620fa267d
batrapulkit <batrapulkit1103@gmail.com>
2026-03-07 19:55:25 -0500

    temp

--- server/quick_delete.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from './src/config/supabase.js';
-
-async function cleanup() {
-    const { error } = await supabase.from('resorts').delete().ilike('name', '%Club Quarters%');
-    if (!error) console.log("Deleted 'Club Quarters' to allow clean re-import.");
-}
-cleanup();
